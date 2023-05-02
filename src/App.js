6a68d0196bb8bb645594a42d0ff845c271a8ed9d
Thomas Skelton <tomskelton@live.co.uk>
2023-05-02 21:54:14 +0100

    imported and added stylesheet

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import CarValue from "./components/CarValue";
 
 
 function App () {
-    return <div>
+    return <div className="container-is-fluid">
         <Carform />
-        <CarList />
         <CarSearch />
+        <CarList />
         <CarValue />
     </div>
 }
